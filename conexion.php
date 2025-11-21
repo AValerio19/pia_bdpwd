a30f3873ace0d4a812fd62f08fba290fa8de9045
AValerio19 <avaleriocasas19@gmail.com>
2025-11-20 21:23:49 -0600

    Conexion.php: se actualizo la bd

--- conexion.php
+++ conexion.php
@@ -1,5 +1,13 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "mariscos_5l3");
+
+$db_host = getenv("DB_HOST");
+$db_user = getenv("DB_USER");
+$db_pass = getenv("DB_PASS");
+$db_name = getenv("DB_NAME");
+$db_port = getenv("DB_PORT");
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
+
 if (!$con) {
-    die("No se establecio la conexion con el servidor". mysqli_error($con));
+    die("Error al conectar: " . mysqli_connect_error());
 }
