270543dabb238f3547fb8a2439623fe1d208e066
AValerio19 <avaleriocasas19@gmail.com>
2025-11-20 21:35:22 -0600

    conexion.php: se actualizo la bd de nuevo

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 <?php
 
-$db_host = getenv("DB_HOST");
-$db_user = getenv("DB_USER");
-$db_pass = getenv("DB_PASS");
-$db_name = getenv("DB_NAME");
-$db_port = getenv("DB_PORT");
-
-$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
+$con = mysqli_connect(
+    getenv("DB_HOST"),
+    getenv("DB_USERNAME"),
+    getenv("DB_PASSWORD"),
+    getenv("DB_NAME"),
+    getenv("DB_PORT")
+);
 
 if (!$con) {
     die("Error al conectar: " . mysqli_connect_error());
