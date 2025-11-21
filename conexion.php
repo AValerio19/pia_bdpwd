b7d60cd7e1a99512babfce1fd48b201fd8ef3196
AValerio19 <avaleriocasas19@gmail.com>
2025-11-21 00:32:19 -0600

    conexion.php fue corregido

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 
 $db_host = getenv("DB_HOST");
 $db_user = getenv("DB_USER");
-$db_pass = getenv("DB_PASS");
+$db_pass = getenv("DB_PASSWORD");
 $db_name = getenv("DB_NAME");
 $db_port = getenv("DB_PORT");
 
